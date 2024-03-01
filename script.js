f1a3cfb2eb14d5d83490e4660fbde4223be92865
Neeleshmishra10 <151340293+Neeleshmishra10@users.noreply.github.com>
2024-03-01 11:56:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+// function hello(){
+//     alert("Hello")
+// }
+
+$(document).ready(function(){
+    // console.log("hello");
+    $(window).scroll(function(){
+        if(this.scrollY > 20)
+        {
+            $('nav').addClass('sticky');
+
+        }
+        else{
+            $('nav').removeClass('sticky');
+              
+        }
+        if(this.scroll > 500){
+            $('.scroll-up-btn').addClass('show');
+        }
+        else{
+            $('.scroll-up-btn').removeClass('show');
+        }
+    });
+    //  slide up script
+     $('.scroll-up-btn').click(function(){
+         $('html').animate({scrollTop: 0});
+    });
+    // toggle menu/nav script
+     $('.menu-btn').click(function(){
+        $('.menu').toggleClass('active');
+        $('.menu-btn i').toggleClass('active');
+   });
+
+//    typing animaion script
+var typed = new Typed(".typing",{
+    strings: ["Developer","Coder","full stack developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+});
+
